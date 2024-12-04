7d67974fceb17e542c01ac802f7d5239607389da
RamK777-stack <ramk97095@gmail.com>
2024-12-04 19:10:31 +0530

    page props issues still..

--- app/blog/page/[page]/page.tsx
+++ app/blog/page/[page]/page.tsx
@@ -4,6 +4,8 @@ import { allBlogs } from 'contentlayer/generated'
 
 const POSTS_PER_PAGE = 5
 
+type Params = Promise<{ page: string }>
+
 export const generateStaticParams = async () => {
   const totalPages = Math.ceil(allBlogs.length / POSTS_PER_PAGE)
   const paths = Array.from({ length: totalPages }, (_, i) => ({ page: (i + 1).toString() }))
@@ -11,7 +13,8 @@ export const generateStaticParams = async () => {
   return paths
 }
 
-export default function Page({ params }: { params: { page: string } }) {
+export default async function Page(props: { params: Params }) {
+  const params = await props.params
   const posts = allCoreContent(sortPosts(allBlogs))
   const pageNumber = parseInt(params.page as string)
   const initialDisplayPosts = posts.slice(
